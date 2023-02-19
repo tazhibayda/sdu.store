e4c6b1a0c9c5767ddc59e076e8734059e2609020
Zanggar Zhumagaliyev <200103287@stu.sdu.edu.kz>
2023-02-19 13:04:06 +0600

    sign up page is ready

--- handlers/authentifaction.go
+++ handlers/authentifaction.go
@@ -1,6 +1,11 @@
 package handlers
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+	"sdu.store/server"
+	"sdu.store/server/model"
+)
 
 func Login(writer http.ResponseWriter, request *http.Request) {
 
@@ -11,7 +16,22 @@ func Logout(writer http.ResponseWriter, request *http.Request) {
 }
 
 func SignUp(writer http.ResponseWriter, request *http.Request) {
-
+	err := request.ParseForm()
+	if err != nil {
+		//danger method
+	}
+	if request.PostFormValue("password") == request.PostFormValue("repassword") {
+		user := model.User{
+			Username: request.PostFormValue("username"),
+			Password: request.PostFormValue("password"),
+		}
+		if err := server.DB.Create(&user); err != nil {
+			//danger method
+		}
+		http.Redirect(writer, request, "/sign-in", 302)
+	} else {
+		//danger method
+	}
 }
 
 func LoginPage(writer http.ResponseWriter, request *http.Request) {
@@ -19,5 +39,6 @@ func LoginPage(writer http.ResponseWriter, request *http.Request) {
 }
 
 func SignUpPage(writer http.ResponseWriter, request *http.Request) {
-
+	t, _ := template.ParseFiles("templates/sign-up.html")
+	t.Execute(writer, nil)
 }
